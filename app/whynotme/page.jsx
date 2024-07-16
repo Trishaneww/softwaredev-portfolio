c513717db1bfa6a267392b1cbc6916c1b339f0eb
Trishane <trishaneww@gmail.com>
2024-07-16 18:28:43 -0400

    whynotme routing folder

---
+++ app/whynotme/page.jsx
@@ -0,0 +1,26 @@
+import '../../styles/global.scss';
+
+
+const PrimeTort = () => {
+  return (
+    <div>
+        <div className="projectPage">
+            <p className="projectPage-title">PrimeTort</p>
+            <img src='/primetort1.png' alt="preview image" />
+
+            <section className="projectPage-gallery2">
+                <img src='/primetort2.png' alt="login page" />
+                <img src='/primetort3.png' alt="login page" />
+                <img src='/primetort4.png' alt="login page" />
+                <img src='/primetort5.png' alt="login page" />
+            </section>
+
+            <footer className="projectPage-nextProject">
+                <a href="/studyhive">StudyHive</a>
+            </footer>
+        </div>
+    </div>
+  )
+}
+
+export default PrimeTort
