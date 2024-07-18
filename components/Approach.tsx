c2ca84a82b329472eb6355b33ea8b69187636002
Trishane <trishaneww@gmail.com>
2024-07-18 15:24:01 -0400

    created phases desc and title

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -11,32 +11,35 @@ const Approach = () => {
     <div>
         <section className="approachContainer">
           <div className="approachContainer-phase flex justify-center items-center p-2 bg-slate-950 relative">
-            <div className="base">
+            <div>
               <Button text={"Phase 1"} />
             </div>
-            <div className="overlay bg-slate-950">
-              <p>Finds campsites using google maps & Weatherbit API to filter through user set parameters.</p>
+            <div className="approachContainer-phase-overlay bg-slate-950">
+              <p className="approachContainer-phase-overlay-title">Planning and Strategy</p>
+              <p>The website development process begins with discovery and planning, where we collaborate to define the website's goals, target audience, key functionalities, and content requirements. Next, I create wireframes and design mockups for approval, ensuring the visual style aligns with your brand.</p>
             </div>
           </div>
 
           <div className="approachContainer-phase flex justify-center items-center p-2 bg-slate-950 relative">
-            <div className="base">
+            <div>
               <Button text={"Phase 2"} />
             </div>
-            <div className="overlay bg-slate-950">
-              <p>Finds campsites using google maps & Weatherbit API to filter through user set parameters.</p>
+            <div className="approachContainer-phase-overlay bg-slate-950">
+              <p className="approachContainer-phase-overlay-title">Development</p>
+              <p>During the development phase, I will code the website using the latest technologies, ensuring robust functionality and optimal performance. This includes implementing responsive design, integrating necessary features, and optimizing for speed and efficiency.</p>
             </div>
           </div>
 
           <div className="approachContainer-phase flex justify-center items-center p-2 bg-slate-950 relative">
-            <div className="base">
+            <div>
               <Button text={"Phase 3"} />
             </div>
-            <div className="overlay bg-slate-950">
-              <p>Finds campsites using google maps & Weatherbit API to filter through user set parameters.</p>
+            <div className="approachContainer-phase-overlay bg-slate-950">
+              <p className="approachContainer-phase-overlay-title">Deployment</p>
+              <p>
+              Once the design is approved, I prepare the website files, configure the hosting environment, and upload the site to a live server. I then provide ongoing maintenance and support to ensure it stays updated, secure, and running smoothly.</p>
             </div>
           </div>
-
         </section>
 
         <MacbookScroll src='/studyhive13.png' />
