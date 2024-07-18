be6d39f070f91994296e9d758178abab17aa5ead
Trishane <trishaneww@gmail.com>
2024-07-18 17:25:40 -0400

    updated footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,19 +13,13 @@ const Footer = () => {
           <p>Elsewhere</p>
           <ul className="footer-contact-list">
             <li>
-              <a href="">Github</a>
+              <a href="https://github.com/Trishaneww">Github</a>
             </li>
             <li>
-              <a href="">Linkedin</a>
+              <a href="https://www.linkedin.com/in/tw11/">Linkedin</a>
             </li>
             <li>
-              <a href="">Dribble</a>
-            </li>
-            <li>
-              <a href="">Email</a>
-            </li>
-            <li>
-              <a href="">Figma</a>
+              <a href="https://www.figma.com/files/team/1301200902848620471/user/1301200898720742294?fuid=1301200898720742294">Figma</a>
             </li>
           </ul>
         </div>
@@ -34,7 +28,7 @@ const Footer = () => {
           <p>Contact</p>
           <ul className="footer-contact-list">
             <li>
-              <a href="">Email</a>
+              <a href="mailto:trishaneww@gmail.com">Email</a>
             </li>
           </ul>
         </div>
